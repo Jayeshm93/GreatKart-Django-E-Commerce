cd272d49ad14347e9cfd3046b4d25a42c5a32709
jayeshmmahajan <jayeshmahajan018@gmail.com>
2026-02-15 23:45:21 +0530

    changes in settings and staticfiles

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -69,7 +69,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    # 'whitenoise.middleware.WhiteNoiseMiddleware',  # <-- Add this line
+    # 'whitenoise.middleware.WhiteNoiseMiddleware',  # <-- serves static files in production
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',

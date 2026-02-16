dcb0cca908a3ae2d156575ad277d8f512ce7f1bd
jayeshmmahajan <jayeshmahajan018@gmail.com>
2026-02-16 14:53:28 +0530

    changes in settings and staticfiles

--- store/models.py
+++ store/models.py
@@ -19,8 +19,7 @@ class Product(models.Model):
         return reverse('product_detail', args=[self.category.slug, self.slug])
 
     def __str__(self):
-        # return self.product_name
-        return f"{self.product_name} - {self.category.name if self.category else 'No Product'}"
+        return self.product_name
 
 
 class VariationManager(models.Manager):
